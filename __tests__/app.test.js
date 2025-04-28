ba22b3250179eea1bb8b9ed075acd9de7d439642
Katy Bennett <katyrs22@gmail.com>
2025-04-28 14:19:53 +0100

    get aticle by Id and tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -48,3 +48,42 @@ describe("GET /api/topics", () => {
       });
   });
 });
+
+describe("GET /api/article/:article_id", () => {
+  test("status 200: responds with requested article", () => {
+      return request(app)
+      .get("/api/article/1")
+      .expect(200)
+      .then(({body}) => {
+          expect(body.article).toMatchObject({
+            article_id: 1,
+            title: "Living in the shadow of a great man",
+            topic: "mitch",
+            author: "butter_bridge",
+            body: "I find this existence challenging",
+            created_at: "2020-07-09T20:11:00.000Z",
+            votes: 100,
+            article_img_url:
+              "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          });
+      });
+  });
+
+  test("status 404 - when passed a valid number but does not exist in the db", () => {
+      return request(app)
+      .get("/api/article/9001")
+      .expect(404)
+      .then(({body}) => {
+          expect(body.msg).toBe("No article found under article id: 9001")
+      });
+  });
+
+  test("status 400 - when passed an invalid article id", () => {
+      return request(app)
+      .get("/api/article/hello")
+      .expect(400)
+      .then(({body}) => {
+          expect(body.msg).toBe("Bad Request!")
+      });
+  });
+});
--- model/model.js
+++ model/model.js
@@ -1,6 +1,25 @@
 const db = require("../db/connection");
 
 module.exports.selectTopics = () => {
-    return db.query("SELECT * FROM topics")
-}
+    return db
+    .query("SELECT * FROM topics")
+};
+
+module.exports.selectArticlesById = (article_id) => {
+    return db
+    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    // .then((result) => {
+    //     return result.rows[0];
+    // })
+    .then(({ rows }) => {
+        if (!rows.length) {
+            return Promise.reject({
+                status: 404, 
+                msg: `No article found under article id: ${article_id}`
+            });
+        };
+        return rows[0];
+    });  
+};
+
 
