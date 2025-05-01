2468fcd62268109dfb1dc0e81969483fe7eb70af
Katy Bennett <katyrs22@gmail.com>
2025-05-01 10:36:21 +0100

    completed task 12, comment count added to response object

--- model/model.js
+++ model/model.js
@@ -3,64 +3,84 @@ const db = require("../db/connection");
 module.exports.selectTopics = () => {
     return db
     .query("SELECT * FROM topics")
-  };
+};
+
   
-  module.exports.selectUsers = () => {
+module.exports.selectUsers = () => {
     return db
     .query("SELECT * FROM users")
-  };
+};
   
-  module.exports.selectArticles = (sortBy = 'created_at', order = 'desc', topic = null ) => {
 
-    const validSortFields = ['created_at', 'article_id', 'title', 'topic', 'author', 'votes', 'comment_count'];
+module.exports.selectArticles = (sortBy = 'created_at', order = 'desc', topic = null ) => {
 
-    if (!validSortFields.includes(sortBy)) {
-        return Promise.reject({
-            status: 400, 
-            msg: `Invalid sort field`
-        });
-    };
+const validSortFields = ['created_at', 'article_id', 'title', 'topic', 'author', 'votes', 'comment_count'];
 
-    const validSortOrders = ['asc', 'desc', 'ASC', 'DESC'];
+if (!validSortFields.includes(sortBy)) {
+    return Promise.reject({
+        status: 400, 
+        msg: `Invalid sort field`
+    });
+};
 
-    if (!validSortOrders.includes(order)) {
-        return Promise.reject({
-            status: 400, 
-            msg: `Invalid sort order`
-        });
-    };
+const validSortOrders = ['asc', 'desc', 'ASC', 'DESC'];
 
-    let where = '';
-    const queryParams = [];
-    if (topic) {
-        where += ` WHERE a.topic = $1`;
-        queryParams.push(topic)
-    }
+if (!validSortOrders.includes(order)) {
+    return Promise.reject({
+        status: 400, 
+        msg: `Invalid sort order`
+    });
+};
 
-    let queryString = `SELECT
-        a.author,
-        a.title,
-        a.article_id,
-        a.topic,
-        a.created_at,
-        a.votes,
-        a.article_img_url,
-        COUNT(c.comment_id)::INT AS comment_count
-        FROM articles AS a
-        LEFT JOIN comments AS c
-        ON c.article_id = a.article_id
-        ${where}
-        GROUP BY a.article_id
-        ORDER BY ${sortBy} ${order}`;
+let where = '';
+const queryParams = [];
+if (topic) {
+    where += ` WHERE a.topic = $1`;
+    queryParams.push(topic)
+}
+
+let queryString = `SELECT
+    a.author,
+    a.title,
+    a.article_id,
+    a.topic,
+    a.created_at,
+    a.votes,
+    a.article_img_url,
+    COUNT(c.comment_id)::INT AS comment_count
+    FROM articles AS a
+    LEFT JOIN comments AS c
+    ON c.article_id = a.article_id
+    ${where}
+    GROUP BY a.article_id
+    ORDER BY ${sortBy} ${order}`;
 
     return db
-        .query(queryString, queryParams)
-        .then(({ rows }) => rows)
-  };
-  
-  module.exports.selectArticlesById = (article_id) => {
+    .query(queryString, queryParams)
+    .then(({ rows }) => rows)
+};
+ 
+
+module.exports.selectArticlesById = (article_id) => {
+    
+let queryString = `SELECT
+    a.author,
+    a.title,
+    a.article_id,
+    a.topic,
+    a.created_at,
+    a.votes,
+    a.body,
+    a.article_img_url,
+    COUNT(c.comment_id)::INT AS comment_count
+    FROM articles AS a
+    LEFT JOIN comments AS c
+    ON c.article_id = a.article_id
+    WHERE a.article_id = $1
+    GROUP BY a.article_id`;
+
     return db
-    .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
+    .query(queryString, [article_id])
     .then(({ rows }) => {
         if (!rows.length) {
             return Promise.reject({
@@ -69,14 +89,15 @@ module.exports.selectTopics = () => {
             });
         };
         return rows[0];
-    });  
-  };
-  
-  module.exports.selectArticleComments = (article_id) => {
+    });
+};
+
+
+module.exports.selectArticleComments = (article_id) => {
     return db
     .query("SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC", [article_id])
     .then((result) => {
-  
+
         if (!result.rows.length) {
             return Promise.reject({
                 status: 404, 
@@ -85,28 +106,30 @@ module.exports.selectTopics = () => {
         };
         return result;
     });    
-  };
-  
-  module.exports.insertComment = (username, body, article_id) => {
+};
+
+
+module.exports.insertComment = (username, body, article_id) => {
     return db
     .query(
-       "INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *",
-       [username, body, article_id] 
+        "INSERT INTO comments (author, body, article_id) VALUES ($1, $2, $3) RETURNING *",
+        [username, body, article_id] 
     )
     .then((result) => {
         return result.rows[0]
-    })
-  }
-  
-  module.exports.updateArticleVoteCount = (inc_votes, article_id) => {
+    });
+};
+
+
+module.exports.updateArticleVoteCount = (inc_votes, article_id) => {
     return db
     .query(`
-      UPDATE articles
-      SET votes = votes + $1
-      WHERE article_id = $2
-      RETURNING *;`,
-      [inc_votes, article_id]
-    )
+        UPDATE articles
+        SET votes = votes + $1
+        WHERE article_id = $2
+        RETURNING *;`,
+        [inc_votes, article_id]
+)
     .then(({rows}) => {
         if (!rows.length) {
             return Promise.reject({
@@ -116,9 +139,10 @@ module.exports.selectTopics = () => {
         };
         return rows[0]
     });  
-  };
-  
-  module.exports.deleteCommentById = (comment_id) => {
+};
+
+
+module.exports.deleteCommentById = (comment_id) => {
     return db
     .query(`
         DELETE FROM comments
@@ -135,5 +159,5 @@ module.exports.selectTopics = () => {
         };
         return rows;
     });
-  };
+};
  
