0a698a692d92d7deea63950e3e3f5cb7aa2e1f06
Katy Bennett <katyrs22@gmail.com>
2025-05-04 16:54:51 +0100

    express routers

---
+++ routes/comments-router.js
@@ -0,0 +1,7 @@
+const { updateCommentById, deleteComment } = require("../controllers/controllers");
+const commentsRouter = require("express").Router();
+
+commentsRouter.patch("/:comment_id", updateCommentById);
+commentsRouter.delete("/:comment_id", deleteComment);
+
+module.exports = commentsRouter;
